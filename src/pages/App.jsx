fb143f51b9c10aa1a33028d426d00a154f2b39fb
trista_lea <tristan_lea@outlook.com>
2023-06-19 14:08:15 -0700

    mvp, ready to show and make changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -6,13 +6,39 @@ import image1 from '../assets/banana_leaves.jpg';
 function App() {
 
   return (
-      <div className="App">
+      <div className="background">
+
       <Parallax pages={1} style={{ top: '0', left: '0' }}>
-        <ParallaxLayer offset={0} speed={0.5}>
-          <img src={image1} alt="banana leaves" />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={2.5}>
-          <h1 className='cardContainer'>Parallax</h1>
+        <ParallaxLayer sticky={{ start: 1}}>
+        <div className="cardContainer">
+          <div className="card">         
+            <div>
+              <h1 className="title">Only Wan Dog Grooming</h1>
+            </div>
+            <div className="textBody">
+              <p>
+                phone: 206-454-0749 <br />
+                Hours of Operation: 9am-5pm, Monday-Friday <br />
+                Email: test@gmail.com <br /> <br />
+                Services:
+              </p>
+              <ol>
+                <li>Hair trimming</li>
+                <li>Bathing</li>
+                <li>Nail trimming</li>
+                <li>Ear cleaning</li>
+                <li>Gland expression (by request)</li>
+              </ol>
+              <p>
+                Price: <br />
+                $80 per hour <br />
+                $20 per 15 minutes <br />
+                Average service time: About 45 minutes to an hour <br />
+              </p>
+            </div>
+          </div>
+        </div>
+        
         </ParallaxLayer>
       </Parallax>
       </div>
@@ -22,30 +48,5 @@ function App() {
 export default App
 
 /*
-<div className="container">
-  <div>
-    <h1 className="title">Only Wan Dog Grooming</h1>
-  </div>
-  <div className="card">
-    <p>
-      phone: 206-454-0749 <br />
-      Hours of Operation: 9am-5pm, Monday-Friday <br />
-      Email: test@gmail.com <br /> <br />
-      Services:
-    </p>
-    <ol>
-      <li>Hair trimming</li>
-      <li>Bathing</li>
-      <li>Nail trimming</li>
-      <li>Ear cleaning</li>
-      <li>Gland expression (by request)</li>
-    </ol>
-    <p>
-      Price: <br />
-      $80 per hour <br />
-      $20 per 15 minutes <br />
-      Average service time: About 45 minutes to an hour <br />
-    </p>
-  </div>
-</div>
+
 */
