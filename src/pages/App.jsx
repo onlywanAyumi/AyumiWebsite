550d4b30422ed7a9a5c798581666e4e0db754351
trista_lea <tristan_lea@outlook.com>
2023-06-23 20:42:55 -0700

    large rework, basically starting over, created logo

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,14 +1,52 @@
-// import '../css/Index.css'
-import '../css/temp.css'
-import {Parallax, ParallaxLayer} from '@react-spring/parallax';
-import image1 from '../assets/banana_leaves.jpg';
+import '../css/Index.css'
+// import '../css/main.css'
+// import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+
+import "../assets/satoLogo1.png"
+
+//create scrollTo function
+
 
 function App() {
 
   return (
-      <div className="background">
+    <div>
+      <div className='title'>
+        large text
+      </div>
+      <div className='subTitle'>
+        Subtitle
+      </div>
 
-      <Parallax pages={1} style={{ top: '0', left: '0' }}>
+      <div className='sectionTitle'>
+        Why Us:
+      </div>
+      <div className='subText'>
+        <p></p>
+      </div>
+
+      <div className='sectionTitle'>
+        Services:
+      </div>
+      <div className='subText'>
+        <p></p>
+      </div>
+
+      <div className='sectionTitle'>
+        Contact:
+      </div>
+      <div className='subText'>
+        <p></p>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
+
+/*
+<Parallax pages={1} style={{ top: '0', left: '0' }}>
         <ParallaxLayer sticky={{ start: 1}}>
         <div className="cardContainer">
           <div className="card">         
@@ -23,17 +61,15 @@ function App() {
                 Services:
               </p>
               <ol>
-                <li>Hair trimming</li>
+                <li>Full groom</li>
+                <li>Partial</li>
                 <li>Bathing</li>
                 <li>Nail trimming</li>
                 <li>Ear cleaning</li>
                 <li>Gland expression (by request)</li>
               </ol>
               <p>
-                Price: <br />
-                $80 per hour <br />
-                $20 per 15 minutes <br />
-                Average service time: About 45 minutes to an hour <br />
+                
               </p>
             </div>
           </div>
@@ -41,12 +77,4 @@ function App() {
         
         </ParallaxLayer>
       </Parallax>
-      </div>
-  )
-}
-
-export default App
-
-/*
-
 */
