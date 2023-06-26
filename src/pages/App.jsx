b72b6c48c8ea204e9f43099d63e1a484ffcdc447
trista_lea <tristan_lea@outlook.com>
2023-06-25 20:35:12 -0700

    reorganizing, and improving the new version

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,80 +1,56 @@
 import '../css/Index.css'
-// import '../css/main.css'
 // import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 
-import "../assets/satoLogo1.png"
+import sato from "../../public/satoLogo1.png"
 
-//create scrollTo function
-
+//create scrollTo function()
 
 function App() {
 
   return (
-    <div>
+    <div className='app'>
+      <div className="card">
       <div className='title'>
         large text
       </div>
       <div className='subTitle'>
         Subtitle
       </div>
+      <img src={"sato"} />
 
       <div className='sectionTitle'>
         Why Us:
       </div>
       <div className='subText'>
-        <p></p>
+        <p>fiuaehiueonhcihaoienfcimcamoifchihifcihm
+          xihidshfoicahihfxiheinxhfimhaihx</p>
       </div>
 
       <div className='sectionTitle'>
         Services:
       </div>
       <div className='subText'>
-        <p></p>
+        <p>1. hufhuehfiwheifwehf
+        2. iufheufhiewhfiuew
+        3. uheiuhdiwhdiuew h
+        4. fuheiwhfiuewhiuhwdiuw
+        5. oifjoejfowjoiejf</p>
       </div>
 
       <div className='sectionTitle'>
         Contact:
       </div>
       <div className='subText'>
-        <p></p>
+        <p>uhewifhwiehfiue fuewhfiehfuwhe
+          fiuwheihfuew fiwheihwif
+          fwehfiuew fewhfiewf
+        </p>
       </div>
-
+      Image by <a href="https://www.freepik.com/free-vector/tea-leaves-background-with-flat-design_2427211.htm#query=leaf%20pattern&position=13&from_view=keyword&track=ais">Freepik</a>
     </div>
+    </div>
+    
   )
 }
 
 export default App
-
-/*
-<Parallax pages={1} style={{ top: '0', left: '0' }}>
-        <ParallaxLayer sticky={{ start: 1}}>
-        <div className="cardContainer">
-          <div className="card">         
-            <div>
-              <h1 className="title">Only Wan Dog Grooming</h1>
-            </div>
-            <div className="textBody">
-              <p>
-                phone: 206-454-0749 <br />
-                Hours of Operation: 9am-5pm, Monday-Friday <br />
-                Email: test@gmail.com <br /> <br />
-                Services:
-              </p>
-              <ol>
-                <li>Full groom</li>
-                <li>Partial</li>
-                <li>Bathing</li>
-                <li>Nail trimming</li>
-                <li>Ear cleaning</li>
-                <li>Gland expression (by request)</li>
-              </ol>
-              <p>
-                
-              </p>
-            </div>
-          </div>
-        </div>
-        
-        </ParallaxLayer>
-      </Parallax>
-*/
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../css/Index.css'
-// import '../css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
