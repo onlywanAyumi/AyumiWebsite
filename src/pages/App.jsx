5f67f7d75bd8eaabfd3b0c2398aa52f1411a269c
trista_lea <tristan_lea@outlook.com>
2023-06-27 11:26:23 -0700

    small changes

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -10,44 +10,44 @@ function App() {
   return (
     <div className='app'>
       <div className="card">
-      <div className='title'>
-        large text
-      </div>
-      <div className='subTitle'>
-        Subtitle
-      </div>
-      <img src={"sato"} />
+        <div className='title'>
+          large text
+        </div>
+        <div className='subTitle'>
+          Subtitle
+        </div>
+        <img src={"sato"} />
 
-      <div className='sectionTitle'>
-        Why Us:
-      </div>
-      <div className='subText'>
-        <p>fiuaehiueonhcihaoienfcimcamoifchihifcihm
-          xihidshfoicahihfxiheinxhfimhaihx</p>
-      </div>
+        <div className='sectionTitle'>
+          Why Us:
+        </div>
+        <div className='subText'>
+          <p>fiuaehiueonhcihaoienfcimcamoifchihifcihm
+            xihidshfoicahihfxiheinxhfimhaihx</p>
+        </div>
 
-      <div className='sectionTitle'>
-        Services:
-      </div>
-      <div className='subText'>
-        <p>1. hufhuehfiwheifwehf
-        2. iufheufhiewhfiuew
-        3. uheiuhdiwhdiuew h
-        4. fuheiwhfiuewhiuhwdiuw
-        5. oifjoejfowjoiejf</p>
-      </div>
+        <div className='sectionTitle'>
+          Services:
+        </div>
+        <div className='subText'>
+          <p>1. hufhuehfiwheifwehf
+          2. iufheufhiewhfiuew
+          3. uheiuhdiwhdiuew h
+          4. fuheiwhfiuewhiuhwdiuw
+          5. oifjoejfowjoiejf</p>
+        </div>
 
-      <div className='sectionTitle'>
-        Contact:
+        <div className='sectionTitle'>
+          Contact:
+        </div>
+        <div className='subText'>
+          <p>uhewifhwiehfiue fuewhfiehfuwhe
+            fiuwheihfuew fiwheihwif
+            fwehfiuew fewhfiewf
+          </p>
+        </div>
+        Image by <a href="https://www.freepik.com/free-vector/tea-leaves-background-with-flat-design_2427211.htm#query=leaf%20pattern&position=13&from_view=keyword&track=ais">Freepik</a>
       </div>
-      <div className='subText'>
-        <p>uhewifhwiehfiue fuewhfiehfuwhe
-          fiuwheihfuew fiwheihwif
-          fwehfiuew fewhfiewf
-        </p>
-      </div>
-      Image by <a href="https://www.freepik.com/free-vector/tea-leaves-background-with-flat-design_2427211.htm#query=leaf%20pattern&position=13&from_view=keyword&track=ais">Freepik</a>
-    </div>
     </div>
     
   )
