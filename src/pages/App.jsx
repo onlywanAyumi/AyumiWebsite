dd525b250114792e7aea081ec5014b3e77334c98
trista_lea <tristan_lea@outlook.com>
2023-06-30 15:47:56 -0700

    new build, todo: logo finish, media queriers css variable for rem, image insert, links to social media

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,10 +1,8 @@
 import '../css/Index.css'
 // import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 
-import sato from "../../public/satoLogo1.png"
-import dog from "../../public/testImage.png"
-
-//create scrollTo function()
+import sato from "../../public/newSato3.png"
+// import dog from "../../public/testImage.png"
 
 //should work as nav bar
 
@@ -14,47 +12,41 @@ function App() {
     <div className='app'>
       <div className="card">
         <div className='titleBar'>
-          <div className='title'>
-            OnlyWan
-          </div>
           <img class="logo" src={sato} />
         </div>
-        <nav>
-          <ul>
-            <li><a href="#whyUs">Why Us</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-        <div className='subTitle'>
-          OnlyWan is low-stress dog grooming service
-        </div>
-        <br/>
+        <section>
+          <div className='sectionTitle'>
+            <h2>Welcome to:</h2>
+          </div>
+          <div className='subTitle'>
+            OnlyWan a low-stress dog grooming service.
+          </div>
+        </section>
         <section>
           <div className='sectionTitle'>
             <h2>Why us:</h2>
           </div>
           <div className='subText'>
             <p>
-              At OnlyWan, we strive to provide a calming, relaxing and welcoming environment 
+              At OnlyWan, we strive to provide a calming, relaxing, and welcoming environment 
               where our furry friends can relax and enjoy a wonderful grooming experience. 
               We understand that each dog has unique needs, characteristics and preference. Whether 
               our furry friend needs a simple bath, brush, or a stylish haircut, we always 
               prioritize their comfort and well-being!
-              <br/>
+              {/* <br/> */}
             </p>
             {/* <img className="dog" src = {dog} /> */}
             <p>
-              <br/>
+              {/* <br/> */}
               Ayumi is not only a groomer, but she also has been studying dog behaviors and animal 
-              welfare for years. She loves and cares dogs, and she will provide a gentle and 
+              welfare for years. She loves and cares for dogs, and she will provide a gentle and 
               compassionate grooming approach, putting the dogs at ease and building trust 
               with dogs during the grooming process!
-              <br/>
+              <br />
+              <br />
+              Ayumi was also the third place winner 
+              of the salon freestyle at the Northwest Groonnubg Show 2023
             </p>
-            <div>
-              image goes here
-            </div>
           </div>
         </section>
         <br />
@@ -63,10 +55,8 @@ function App() {
             <h2>Services:</h2>
           </div>
           <div className='subText'>
+            <h4>For puppies:</h4>
             <p>
-              For puppies:
-              <br/>
-              <br/>
               The first grooming experience for puppy is very important 
               because It’s are not only one time job but life-long commitment.
               They have no idea what they’ll experience in their first time therefore we will 
@@ -74,38 +64,59 @@ function App() {
               for them which will continually no stress for rest of them grooming life!
               <br/>
               <br/>
-              The Teddy bear haircut:
-              <br/>
-              <br/>
+            </p>
+            <h4>The Teddy Bear haircut:</h4>
+            <p>
               Do you like to have your dog to be looks like teddy bear?
               We will seek out each dog’s potential and make as cute as possible!
-            
-              Asian-fusion haircut
-
+              <br />
+              <br />
+            </p>
+            <h4>Asian-fusion haircut</h4>
+            <p>
               They have variety of asian-fusion hairstyles.
               If you look for your pet to have special looking, 
               we are more than welcome to consult for it.
+              <br/>
+              <br/>
+            </p>
+            <h4>Standard Full Grooming:</h4>
+            <p>
+              Full grooming includes bath, brush, haircut, 
+              nail trim, ear cleaning, 
+              and anal gland expression (by request).
             </p>
           </div>
         </section>
         <br />
         <section>
-        <div className='sectionTitle'>
-          <h2>Contact:</h2>
-        </div>
-        <div className='subText'>
-          <p>
-            email: 
+          <div className='sectionTitle'>
+            <h2>Contact:</h2>
+          </div>
+          <div className='subText'>
+            Email: 
+            OnlyWanDogs@gmail.com
             <br/>
-            Number:
-          </p>
-        </div>
+            Number: 
+            <br />
+            206-454-0749
+          </div>
         </section>
-        Image by <a href="https://www.freepik.com/free-vector/tea-leaves-background-with-flat-design_2427211.htm#query=leaf%20pattern&position=13&from_view=keyword&track=ais">Freepik</a>
       </div>
     </div>
-    
   )
 }
 
 export default App
+
+/*
+idea for scroll to function, may be best for mobile
+do to large card length
+
+<nav>
+  <ul>
+    <li><a href="#whyUs">Why Us</a></li>
+    <li><a href="#services">Services</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+</nav> */
