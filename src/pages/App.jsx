a3fb49316d45f3c98ee5954e1344a0e2c3ce6862
trista_lea <tristan_lea@outlook.com>
2023-07-07 18:54:40 -0700

    changed all file types, added video

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,21 @@
 import '../css/Index.css'
 // import {Parallax, ParallaxLayer} from '@react-spring/parallax';
 
-import sato from "../../public/sotaLogos/sotaLogo9.png"
+import sato from "../../public/sotaLogos/sotaLogo1.webp"
+//change to 8 for other logo!                  ^^^
 
 //photos for body
-import mainImage from "../../public/works/asianFusion.png"
-import puppies from "../../public/works/puppies2.png"
-import teddy from "../../public/works/teddy.png"
-import ayuComp from "../../public/works/ayuComp.jpg"
-import asianFusion from "../../public/works/asianFusion3.png"
-import petal from "../../public/works/petal.png"
+import mainImage from "../../public/works/asianFusion.webp"
+import puppies from "../../public/works/puppies2.webp"
+import teddy from "../../public/works/teddy.webp"
+import ayuComp from "../../public/works/ayuComp.webp"
+import asianFusion from "../../public/works/asianFusion3.webp"
+import petal from "../../public/works/petal.webp"
+import ayu from "../../public/works/AyumiMatsu2.webp"
 
-
+//videos for body
+import afVideo from "../../public/works/afVideo.webm"
+import afVideo2 from "../../public/works/afVideo.mp4"
 
 function App() {
 
@@ -21,6 +25,7 @@ function App() {
         <div className='titleBar'>
           <img className="logo" src={sato} />
         </div>
+        <article>
         <section>
           <div className='sectionTitle'>
             <h2>Welcome to:</h2>
@@ -29,7 +34,7 @@ function App() {
             OnlyWan a low-stress dog grooming service.
           </div>
           <br />
-          <img className="basicPhoto" src={mainImage} />
+          <a href="https://www.instagram.com/ayumi_doggrooming/"><img className="basicPhoto" src={mainImage} /></a>
         </section>
         <section>
           <div className='sectionTitle'>
@@ -43,7 +48,7 @@ function App() {
               our furry friend needs a simple bath, brush, or a stylish haircut, we always 
               prioritize their comfort and well-being!
             </p>
-            <img className='basicPhoto' src={ayuComp} />
+            <a href="https://www.instagram.com/ayumi_doggrooming/"><img className='basicPhoto' src={ayuComp} /></a>
             <p>
               Ayumi is a japanese proffessional dog groomer in seattle who specializes in asian-fusion hairstyles. 
               She is not only a groomer, but she also has been studying dog behaviors and animal
@@ -64,7 +69,7 @@ function App() {
           </div>
           <div className='subText'>
             <h4>For puppies:</h4>
-            <img className="basicPhoto" src={asianFusion} />
+            <a href="https://www.instagram.com/ayumi_doggrooming/"><img className="basicPhoto" src={asianFusion} /></a>
             <p>
               The first grooming experience for puppy is very important 
               because It’s not only a one time job but life-long commitment.
@@ -75,7 +80,7 @@ function App() {
               <br/>
             </p>
             <h4>The Teddy Bear haircut:</h4>
-            <img className="basicPhoto" src={teddy} />
+            <a href="https://www.instagram.com/ayumi_doggrooming/"><img className="basicPhoto" src={teddy} /></a>
             <p>
               Do you like to have your dog to be looks like teddy bear?
               We will seek out each dog’s potential and make as cute as possible!
@@ -83,11 +88,16 @@ function App() {
               <br />
             </p>
             <h4>Asian-fusion haircut</h4>
-            <img className="basicPhoto" src={asianFusion} />
+            {/* <a href="https://www.instagram.com/ayumi_doggrooming/"><video className='basicVideo' src={afVideo} /></a> */}
+            {/* <video className='basicVideo' src={afVideo} /> */}
+            <video className='basicVideo' loop autoPlay muted>
+              <source src={afVideo} type="video/webm" />
+              <source src={afVideo2} type="video/mp4" />
+            </video>
             <p>
-              They have variety of asian-fusion hairstyles.
-              If you look for your pet to have special looking, 
-              we are more than welcome to consult for it.
+              There are a variety of asian-fusion hairstyles.
+              If you looking for your pet to have a special look, 
+              we are more than happy to consult about this.
               <br/>
               {/* <br/> */}
             </p>
@@ -130,6 +140,7 @@ function App() {
             </p>
           </div>
         </section>
+        </article>
         <section>
           <div className='sectionTitle'>
             <h2>Schedule:</h2>
@@ -140,6 +151,7 @@ function App() {
             Monday through Saturday
           </div>
         </section>
+        <br />
         <section>
           <div className='sectionTitle'>
             <h2>Contact:</h2>
@@ -155,10 +167,36 @@ function App() {
             <br/>
             Email: 
             <br />
-            OnlyWanDogs@gmail.com
+            <a href="mailto: OnlyWanDogs@gmail.com">OnlyWanDogs@gmail.com</a>
             <br/>
           </div>
-        </section>   
+        </section>
+        <br />
+        <section>
+          <div className='sectionTitle'>
+            <h2>Qualifications:</h2>
+          </div>
+          <div className='subText'>
+
+            <p className="name">Ayumi Matsumoto</p> 
+            <a href="https://www.instagram.com/ayumi_doggrooming/"><img className='basicPhoto' src={ayu} /></a>
+            
+            <p className='name'>North West Grooming Show 2023</p>
+            
+            <p className='name'>Dog Emotion and Cognition</p>
+            
+            {/* <p className='name'>Duke University</p> */}
+            Duke University
+            
+            <p className='name'>Animal Behaviour and Welfare</p>
+            
+            {/* <p className='name'>University of Edinburgh</p> */}
+            University of Edinburgh
+            
+            <p className='name'>EDIVET Acknowledge of what it takes to be a veterinarian</p>
+            
+          </div>
+        </section>
       </div>
     </div>
   )
