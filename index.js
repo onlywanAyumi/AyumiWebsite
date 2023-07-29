f091bcdcaa08462e0645e91012822d8dbca2f00a
tristan <tlea5@uw.com>
2023-07-29 09:54:52 -0700

    not done but made a lot of progress, many more things are properly styled, beginning to make a nav bar, and debugging a lot of error

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function(){
     var scrollTop = window.scrollY;
-    document.body.style.backgroundPositionY = scrollTop * 0.5 + "px";
+    document.body.style.backgroundPositionY = scrollTop * 0.8 + "px";
 });
