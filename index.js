0eeda9eca75eaeb982d676627cb243478ad059c1
tristan <tlea5@uw.com>
2023-07-29 11:14:22 -0700

    finsished styling, want to add better seo and what not later

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ window.addEventListener("scroll", function(){
     var scrollTop = window.scrollY;
     document.body.style.backgroundPositionY = scrollTop * 0.8 + "px";
 });
+
+// document.querySelectorAll('nav a').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+        
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
