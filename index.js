16ebe562e1fe38a2adf7e5e90796384cc6719434
tristan <tlea5@uw.com>
2023-08-05 16:36:58 -0700

    minor cleaning

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-window.addEventListener("scroll", function(){
-    var scrollTop = window.scrollY;
-    document.body.style.backgroundPositionY = scrollTop * 0.8 + "px";
-});
+// window.addEventListener("scroll", function(){
+//     var scrollTop = window.scrollY;
+//     document.body.style.backgroundPositionY = scrollTop * 0.8 + "px";
+// });
 
 // document.querySelectorAll('nav a').forEach(anchor => {
 //     anchor.addEventListener('click', function (e) {
